948a86aa44e9f31015aecfec19db3f1a282be31a
lucas s <lucassouza.zip@gmail.com>
2022-07-17 00:15:06 -0300

    exceção personalizada

--- src/main/java/com/store/bookstore/service/CategoriaService.java
+++ src/main/java/com/store/bookstore/service/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.store.bookstore.domain.Categoria;
 import com.store.bookstore.repositories.CategoriaRepository;
+import com.store.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado! (Id: "+ id +", tipo: "+ Categoria.class.getName()+")"));
 	}
-	
+
 }
