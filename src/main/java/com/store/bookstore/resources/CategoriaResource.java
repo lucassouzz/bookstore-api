207bbe531cd0b977c6b3cab949f9f645acd3899d
lucas s <lucassouza.zip@gmail.com>
2022-07-17 10:18:17 -0300

    findAll para categoria utilizando padrao DTO

--- src/main/java/com/store/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/store/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.store.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.bookstore.domain.Categoria;
+import com.store.bookstore.dtos.CategoriaDTO;
 import com.store.bookstore.service.CategoriaService;
 
 @RestController
@@ -21,7 +25,13 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 	}
 }
--- src/main/java/com/store/bookstore/service/CategoriaService.java
+++ src/main/java/com/store/bookstore/service/CategoriaService.java
@@ -1,8 +1,10 @@
 package com.store.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.store.bookstore.domain.Categoria;
@@ -19,5 +21,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado! (Id: "+ id +", tipo: "+ Categoria.class.getName()+")"));
 	}
+	
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+		
 
 }
